96836e300244459d50dec40ca3bc1ea5b301f22e
Gauthier de Lesquen <gauthier2lesquen@gmail.com>
2025-12-10 11:33:39 +0100

    Create Cobol.cbl

---
+++ Cobol.cbl
@@ -0,0 +1,9 @@
+      *Warning: Respect the indentation exactly (down to each space)
+
+       IDENTIFICATION DIVISION.
+      *name of your file
+       PROGRAM-ID. Cobol.
+       AUTHOR. ME.
+
+       PROCEDURE DIVISION.
+           DISPLAY 'HELLO, WORLD!'.
